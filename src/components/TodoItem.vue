e65dc0dbba90a8c0b562ac9133607ec7ed92704b
Amir <amirrezwanoori@gmail.com>
2023-01-21 16:04:36 +0330

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-list bordered separator>
+    <q-item>
+      <q-item-section>Single line item</q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    {{ TodoList }}
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import TodoList from 'src/components/TodoList.vue';
+</script>
